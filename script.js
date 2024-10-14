9bda0b08710231b4bd7eb765623c5d1ea6ed77c4
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-14 16:49:03 +0200

    Explaining of the description test

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 console.log('Hello World')
 
 /* 
-
 Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -21,5 +20,26 @@ sconto = prezzo * 40 / 100
 
 lo sconto e il prezzo scontato sono due cose diverse
 
+Descrizione del problema
+
+utente sceglierà 2 dati in ingresso: 
+numeri di chilometri e numero del'età del passeggero.
+
+come uscita
+Calcolare il prezzo totale del viaggio sapendo che:
+il prezzo base è definito in base ai km (0.21 € al km)
+al prezzo del bigletto verrà applicato lo sconto
+
+    sconto = 20% 
+    se l' utente è minorenne  
+
+    sconto = 40% 
+    se l' utente è over 65
+
+    
+il prezzo finale è dato dalla differenza tra 
+il prezzo base e il prezzo scontato. 
+il prezzo corrispettivo sara indicato in euro 
+max 2 cifre dopo la virgola.  Es. euro 39,50  
 
 */
