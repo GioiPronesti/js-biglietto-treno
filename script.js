335069d267df827e74d1b93af22227f0251f5db4
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-14 16:13:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+console.log('Hello World')
+
+/* 
+
+Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L’output del prezzo finale va messo fuori in forma umana con massimo due decimali
+
+lo sconto da applicare si calcola in questo modo:
+se dalla variabile prezzo voglio calcolarmi 
+
+uno sconto del 20% 
+sconto = prezzo * 20 / 100
+
+uno sconto del 40% 
+sconto = prezzo * 40 / 100
+
+lo sconto e il prezzo scontato sono due cose diverse
+
+
+*/
